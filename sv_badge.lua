ea0b4eee74fec6f4637b66c7008fd9d2e1c256c7
Frank_Viking <44592288+Klent45@users.noreply.github.com>
2022-09-03 21:38:39 +0200

    add callsign, grade name and birthdate

--- sv_badge.lua
+++ sv_badge.lua
@@ -5,8 +5,10 @@ AddEventHandler('badge:open', function(ID, targetID, type)
 	local Player = QBCore.Functions.GetPlayer(ID)
 
 	local data = {
-		name = Player.PlayerData.charinfo.firstname.." "..Player.PlayerData.charinfo.lastname,
-		dob = Player.PlayerData.charinfo.dob
+		dob = "LSPD : "..Player.PlayerData.job.grade.name;
+		mat = "Matricule : "..Player.PlayerData.metadata.callsign; --Change "Matricule :" to your language
+		name = Player.PlayerData.charinfo.firstname.." "..Player.PlayerData.charinfo.lastname;
+		birthdate = "DDN : "..Player.PlayerData.charinfo.birthdate; --Change DDN "Date de naissance" to your language (for exemple DOB "Date of birthdate")
 	}
 
 	TriggerClientEvent('badge:open', targetID, data)
